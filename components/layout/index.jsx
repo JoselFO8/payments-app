0791470137ecb6b2768005bfdf448e61bcc99865
Jose <joseluisfo_@hotmail.com>
2022-10-29 02:39:00 -0500

    Styles are finished in the left-nav component

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -2,44 +2,53 @@ import Head from "next/head"
 
 // Components
 import Navbar from "./navBar/index.jsx"
-import Footer from "./footer/index.jsx"
+// import Footer from "./footer/index.jsx"
 import LeftNav from "./left-nav/index.jsx"
 
 export default function Layout (props) {
     // Estado + bolean + activar o desactivar left-nav mediante dos clases en el cual intervenga un grid ancho-leftnav + auto
 
     return (
-        <div className="layout-container">
+        <div className="layout">
             <Head>
                 <title>Head desde layout</title>
                 <meta name="description" content="Contenido para payments-app"/>
             </Head>
             
-            {/* Left Nav */}
-            <div className="layout-container__left-nav">
-                <LeftNav/>
-            </div>
+            <div className="layout-container">
 
-            {/* Layout */}
-            <div className="layout-container__body">
-                <header>
-                    <Navbar/>
-                </header>
-
-                <main>
-                    <div className="layout-container__body-base">
-                        {/* Panel */}
-                        <div className="layout-container__body-base-panel">
-                            {props.children}
-                        </div>
-                    </div>
-                </main>
-
-                {/* <footer className="layout-container__footer">
-                    <Footer/>
-                </footer> */}
-            </div>
             
+                {/* Left Nav */}
+                <div className="layout-container__left-nav">
+                    <LeftNav/>
+                </div>
+
+                {/* Layout */}
+                <div className="layout-container__full">
+                    <header>
+                        <div className="layout-container__full-navbar">
+                            <Navbar/>
+                        </div>
+                    </header>
+
+
+                    <main>
+                        <div className="layout-container__full-base">
+                            {/* Panel */}
+                            <div className="layout-container__full-base-panel">
+                                {props.children}
+                            </div>
+                        </div>
+                    </main>
+
+                    {/* <footer className="layout-container__footer">
+                        <Footer/>
+                    </footer> */}
+                </div>
+
+            
+            </div>     
+
         </div>
     )
 }
--- components/layout/left-nav/index.jsx
+++ components/layout/left-nav/index.jsx
@@ -4,14 +4,54 @@ export default function LeftNav () {
     return (
         <div className="left-nav-container">
             <div className="left-nav-container__slogan">
-                <Image 
-                    src="/slogan/Slogan.png"
-                    alt="Payments slogan"
-                    width={40}
-                    height={40}
-                />
+                <a href='/performance'>
+                    <Image 
+                        src="/slogan/Slogan.png"
+                        alt="Payments slogan"
+                        width={40}
+                        height={45}
+                    />
+                </a>
             </div>
             
+            <div className="left-nav-container__icons">
+                <a href='#!'>
+                    <Image 
+                        src="/icons/home.png"
+                        alt="Notifications"
+                        width={33}
+                        height={35}
+                        className="navbar-container__icons-icon"
+                    />
+                </a>
+                <a href='#!'>
+                    <Image 
+                        src="/icons/calendar.png"
+                        alt="Notifications"
+                        width={30}
+                        height={34}
+                        className="navbar-container__icons-icon"
+                    />
+                </a>
+                <a href='#!'>
+                    <Image 
+                        src="/icons/invoice.png"
+                        alt="Notifications"
+                        width={30}
+                        height={31}
+                        className="navbar-container__icons-icon"
+                    />
+                </a>
+                <a href='#!'>
+                    <Image 
+                        src="/icons/bag.png"
+                        alt="Notifications"
+                        width={30}
+                        height={33}
+                        className="navbar-container__icons-icon"
+                    />
+                </a>
+            </div>
         </div>
         
     )
